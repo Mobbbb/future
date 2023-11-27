1c899351d7362e0c895c9d790c2fc9c2814fd4bb
Mobbbb <mobbbb.top@gmail.com>
2023-11-27 17:10:17 +0800

    echart图表优化

--- src/single-page/order/option.js
+++ src/single-page/order/option.js
@@ -128,6 +128,10 @@ function getYAxisIndexSeries(series, dataSource, option) {
     }
 }
 
+function circleDom(color, size, left = 0, right = 4) {
+    return `<span style="display: inline-block; width: ${size}px; height: ${size}px; border-radius: 50%; background: ${color}; margin: 0 ${right}px 0 ${left}px;vertical-align: middle;"></span>`
+}
+
 export const getBarOption = (data) => {
     let sourceData = []
     Object.keys(data).forEach(key => {
@@ -396,10 +400,6 @@ export const getDoubleKLineOption = (params, config) => {
                     saleDate2 = `(${lineXAxisInfo2[saleItem.data[0]][0].date.slice(5)})`
                 }
 
-                function circleDom(color, size, left = 0, right = 4) {
-                    return `<span style="display: inline-block; width: ${size}px; height: ${size}px; border-radius: 50%; background: ${color}; margin: 0 ${right}px 0 ${left}px;vertical-align: middle;"></span>`
-                }
-
                 return `
                         <div style="border: 1px solid rgb(255, 255, 255);">
                             <div style="line-height: 1; margin-top: 4px;">${circleDom('#eb5454', 10)}<span>多单</span></div>          
@@ -559,160 +559,6 @@ export const getDoubleKLineOption = (params, config) => {
     }
 }
 
-export const getKLineOption = (params) => {
-    const { x, y, year, name, type } = params
-
-    let totalPrice = []
-    y.forEach(item => totalPrice = totalPrice.concat(item))
-    
-    let compareDate = ''
-    if (type === 0) {
-        compareDate = CHUNJIE_YEAR_MAP[year]
-    } else if (type === 1) {
-        compareDate = '10-01'
-    } else {
-        compareDate = '05-01'
-    }
-
-    let chunjieDate = ''
-    for (let i = 0; i < x.length; i++) {
-        if (compareDate < x[i]) {
-            chunjieDate = x[i - 1] || ''
-            break
-        }
-    }
-
-    let combineArr = new Set(totalPrice)
-    combineArr.delete(null)
-    let min = Math.min(...combineArr)
-    const minLength = String(min).length
-    let formatRate = Math.pow(10, (minLength - 2))
-    formatRate = formatRate < 1 ? 1 : formatRate
-    min = Math.floor(min / formatRate) * formatRate
-
-    return {
-        tooltip: {
-            trigger: 'axis',
-        },
-        axisPointer: {
-            show: true,
-        }, 
-        title: {
-            text: `${year.slice(0, 4)}年${festivalMap[type]}`,
-            subtext: `${name.slice(name.length - 2, name.length)}合约`,
-            x: 'center',
-            itemGap: 5,
-        },
-        grid: [
-            {
-                left: 40,
-                right: 20,
-                bottom: '10%',
-                top: 50,
-                containLabel: false,
-            },
-        ],
-        xAxis: [
-            {
-                type: 'category',
-                axisLine: {
-                    onZero: true,
-                    lineStyle: {
-                        color: '#d0d0d0',
-                        width: 1,
-                    },
-                },
-                axisLabel: {
-                    textStyle: {
-                        color: '#8e8e8e',
-                        fontSize,
-                    },
-                },
-                data: x,
-            },
-        ],
-        yAxis: [
-            {
-                name: '',
-                type: 'value',
-                nameGap: 20,
-                boundaryGap: false,
-                min,
-                max: function (value) {
-                    return parseInt((value.max / 100)) * 100 + 200
-                },
-                axisLabel: {
-                    textStyle: {
-                        color: '#8e8e8e',
-                        fontSize,
-                    },
-                },
-                splitLine: {
-                    show: true,
-                    lineStyle: {
-                        color: '#f1f3f8',
-                        type: 'dashed',
-                    },
-                },
-            },
-        ],
-        series: [
-            {
-                name: '多单',
-                type: 'candlestick',
-                data: y,
-                markLine: {
-                  symbol: ['none', 'none'],
-                  data: [
-                        [
-                            {
-                                name: 'from lowest to highest',
-                                type: 'min',
-                                valueDim: 'lowest',
-                                symbol: 'circle',
-                                symbolSize: 0,
-                                label: {
-                                    show: false
-                                },
-                                emphasis: {
-                                    label: {
-                                        show: false
-                                    }
-                                }
-                            },
-                            {
-                                type: 'max',
-                                valueDim: 'highest',
-                                symbol: 'circle',
-                                symbolSize: 0,
-                                label: {
-                                    show: false
-                                },
-                                emphasis: {
-                                    label: {
-                                        show: false
-                                    }
-                                }
-                            },
-                        ],
-                        {
-                            xAxis: chunjieDate,
-                            lineStyle: {
-                                color: '#d0d0d0',
-                                opacity: 0.5,
-                            },
-                            label: {
-                                position: 'end',
-                                distance: [0, -20],
-                            },
-                        },
-                    ],
-                },
-            },
-        ],
-    }
-}
-
 export const getOrderLineOption = (data) => {
     return {
         tooltip: {
@@ -813,6 +659,238 @@ export const getOrderLineOption = (data) => {
     }
 }
 
+export const getKLineOption = (params) => {
+    const { x, y, year, name, type } = params
+
+    let totalPrice = []
+    y.forEach(item => totalPrice = totalPrice.concat(item))
+    
+    let compareDate = ''
+    if (type === 0) {
+        compareDate = CHUNJIE_YEAR_MAP[year]
+    } else if (type === 1) {
+        compareDate = '10-01'
+    } else {
+        compareDate = '05-01'
+    }
+
+    let chunjieDate = ''
+    for (let i = 0; i < x.length; i++) {
+        if (compareDate < x[i]) {
+            chunjieDate = x[i - 1] || ''
+            break
+        }
+    }
+
+    let combineArr = new Set(totalPrice)
+    combineArr.delete(null)
+    let min = Math.min(...combineArr)
+    const minLength = String(min).length
+    let formatRate = Math.pow(10, (minLength - 2))
+    formatRate = formatRate < 1 ? 1 : formatRate
+    min = Math.floor(min / formatRate) * formatRate - formatRate
+
+    return {
+        tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+                animation: false
+            },
+            className: 'k-line-tooltip',
+            formatter: function(data) {
+                const color = data[0].value[2] > data[0].value[1] ? '#eb5454' : '#47b262'
+
+                return `
+                        <div style="border: 1px solid rgb(255, 255, 255);">        
+                            <div style="line-height: 1;">${circleDom(color, 9, 3, 7)}开盘
+                                <span style="font-weight: bold;margin-left: 20px; float: right;">${data[0].value[1]}</span>
+                            </div>
+                            <div style="line-height: 1; margin-top: 10px;">${circleDom(color, 9, 3, 7)}收盘
+                                <span style="font-weight: bold;margin-left: 20px; float: right;">${data[0].value[2]}</span>
+                            </div>
+                            <div style="line-height: 1; margin-top: 10px;">${circleDom(color, 9, 3, 7)}最低
+                                <span style="font-weight: bold;margin-left: 20px; float: right;">${data[0].value[3]}</span>
+                            </div>
+                            <div style="line-height: 1; margin-top: 10px;">${circleDom(color, 9, 3, 7)}最高
+                                <span style="font-weight: bold;margin-left: 20px; float: right;">${data[0].value[4]}</span>
+                            </div>
+                        </div>
+                `
+            },
+            position: function (pos, params, dom, rect, size) {
+                var obj = { top: 100 }
+                if ((pos[0] < size.viewSize[0] / 2)) {
+                    obj.right = 5
+                } else {
+                    obj.left = 44
+                }
+                return obj
+            },
+        },
+        axisPointer: {
+            show: true,
+            label: {
+                precision: 0,
+                padding: [5, 5, 5, 5],
+            },
+        }, 
+        title: {
+            text: `${year.slice(0, 4)}年${festivalMap[type]}`,
+            subtext: `${name.slice(name.length - 2, name.length)}合约`,
+            x: 'center',
+            itemGap: 5,
+        },
+        grid: [
+            {
+                left: 40,
+                right: 20,
+                bottom: '10%',
+                top: 50,
+                containLabel: false,
+            },
+        ],
+        xAxis: [
+            {
+                type: 'category',
+                axisLine: {
+                    onZero: true,
+                    lineStyle: {
+                        color: '#d0d0d0',
+                        width: 1,
+                    },
+                },
+                axisLabel: {
+                    textStyle: {
+                        color: '#8e8e8e',
+                        fontSize,
+                    },
+                },
+                data: x,
+            },
+        ],
+        yAxis: [
+            {
+                name: '',
+                type: 'value',
+                nameGap: 20,
+                boundaryGap: false,
+                min,
+                max: function (value) {
+                    return parseInt((value.max / 100)) * 100 + 250
+                },
+                axisLabel: {
+                    textStyle: {
+                        color: '#8e8e8e',
+                        fontSize,
+                    },
+                },
+                splitLine: {
+                    show: true,
+                    lineStyle: {
+                        color: '#f1f3f8',
+                        type: 'dashed',
+                    },
+                },
+            },
+        ],
+        series: [
+            {
+                name: '多单',
+                type: 'candlestick',
+                data: y,
+                markPoint: {
+                    label: {
+                        formatter: function (param) {
+                            return param != null ? Math.round(param.value) + '' : '';
+                        }
+                    },
+                    data: [
+                        {
+                            name: 'highest value',
+                            type: 'max',
+                            valueDim: 'highest',
+                            itemStyle: {
+                                color: 'transparent',
+                            },
+                            label: {
+                                textStyle: {
+                                    color: '#eb5454',
+                                    fontSize: 10,
+                                },
+                                position: 'insideBottom',
+                                distance: 0,
+                            },
+                        },
+                        {
+                            name: 'lowest value',
+                            type: 'min',
+                            valueDim: 'lowest',
+                            itemStyle: {
+                                color: 'transparent',
+                            },
+                            label: {
+                                textStyle: {
+                                    color: '#47b262',
+                                    fontSize: 10,
+                                },
+                                position: 'insideBottom',
+                                distance: -14,
+                            },
+                        },
+                    ],
+                },
+                markLine: {
+                  symbol: ['none', 'none'],
+                  data: [
+                        [
+                            {
+                                name: 'from lowest to highest',
+                                type: 'min',
+                                valueDim: 'lowest',
+                                symbol: 'circle',
+                                symbolSize: 0,
+                                label: {
+                                    show: false
+                                },
+                                emphasis: {
+                                    label: {
+                                        show: false
+                                    }
+                                }
+                            },
+                            {
+                                type: 'max',
+                                valueDim: 'highest',
+                                symbol: 'circle',
+                                symbolSize: 0,
+                                label: {
+                                    show: false
+                                },
+                                emphasis: {
+                                    label: {
+                                        show: false
+                                    }
+                                }
+                            },
+                        ],
+                        {
+                            xAxis: chunjieDate,
+                            lineStyle: {
+                                color: '#d0d0d0',
+                                opacity: 0.5,
+                            },
+                            label: {
+                                position: 'end',
+                                distance: [0, -20],
+                            },
+                        },
+                    ],
+                },
+            },
+        ],
+    }
+}
+
 export const getArrLineOption = (data, name) => {
     const series = []
     let xAxis = []
@@ -859,11 +937,44 @@ export const getArrLineOption = (data, name) => {
         legend: {
             bottom: 10,
         },
+        axisPointer: {
+            show: true,
+            label: {
+                precision: 0,
+                padding: [5, 5, 5, 5],
+            },
+        },
         title: {
             text: `${name < 10 ? '0' + name : name}合约`,
             x: 'center',
         },
-        tooltip: { trigger: 'axis' },
+        tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+                animation: false
+            },
+            className: 'k-line-tooltip',
+            formatter: function(data) {
+                let str = ''
+                for (let i = data.length - 2; i > -1; i --) {
+                    const marginTop = i === data.length - 2 ? '' : 'margin-top: 10px;'
+                    str += `<div style="line-height: 1;${marginTop}">${circleDom(data[i].color, 9, 3, 7)}${data[i].data.date}
+                                <span style="font-weight: bold;margin-left: 20px; float: right;">${data[i].value}</span>
+                            </div>`
+                }
+
+                return `<div style="border: 1px solid rgb(255, 255, 255);">${str}</div>`
+            },
+            position: function (pos, params, dom, rect, size) {
+                var obj = { top: 100 }
+                if ((pos[0] < size.viewSize[0] / 2)) {
+                    obj.right = 5
+                } else {
+                    obj.left = 44
+                }
+                return obj
+            },
+        },
         xAxis: [{
             type: 'category',
             boundaryGap: false,
